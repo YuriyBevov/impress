6f65873bbb5551ba7126ff3e2fa47b6bf9aa3f73
Yuriy Bevov <yuriybevov@gmail.com>
2024-12-18 19:57:25 +0300

    Добавил аккордеон, убрал стандартные карточки в компонент base-card

--- src/pug/components/_ui/accordeon/accordeon.js
+++ src/pug/components/_ui/accordeon/accordeon.js
@@ -1,4 +1,3 @@
-console.log('accordeon');
 const accordeons = document.querySelectorAll('.accordeon');
 
 if (accordeons) {
@@ -7,14 +6,7 @@ if (accordeons) {
 
     items.forEach((item) => {
       item.addEventListener('click', () => {
-        console.log('click');
-        items.forEach((item) => {
-          item.parentNode.classList.contains('expanded') && item.parentNode.classList.remove('expanded');
-        });
-
-        setTimeout(() => {
-          item.parentNode.classList.toggle('expanded');
-        }, 0);
+        item.parentNode.classList.toggle('expanded');
       });
     });
   });
