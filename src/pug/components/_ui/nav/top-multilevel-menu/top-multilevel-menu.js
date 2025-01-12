cfeedecf051e94ee0921345dc6dddd4c4aee1cd2
Yuriy Bevov <yuriybevov@gmail.com>
2025-01-12 19:43:17 +0300

    Шапка и выпадающее меню

--- src/pug/components/_ui/nav/top-multilevel-menu/top-multilevel-menu.js
+++ src/pug/components/_ui/nav/top-multilevel-menu/top-multilevel-menu.js
@@ -1,42 +1,48 @@
+import {gsap} from 'gsap';
+const menu = document.querySelector('.top-multilevel-menu');
+const wrapper = document.querySelector('.top-multilevel-menu__wrapper');
+const inners = menu.querySelectorAll('.has-inner');
+console.log('test');
 
-const items = document.querySelectorAll('.main-nav__list-item > a > svg');
-const innerItems = document.querySelectorAll('.top-multilevel-menu__list-item > a > svg');
+if(menu) {
+  const tl = gsap.timeline().pause();
 
-if(items.length) {
-  items.forEach(item => {
-    item.addEventListener('click', (evt) => {
-      evt.preventDefault();
-      evt.currentTarget.closest('.main-nav__list-item').classList.toggle('expanded');
-    });
+  tl.fromTo(wrapper, {
+    visibility: 'hidden',
+    y: -5,
+    opacity: 0
+  },
+  {
+    visibility: 'visible',
+    y: 0,
+    duration: 0.3,
+    opacity: 1
+  });
+
+  menu.addEventListener('mouseenter', (evt) => {
+    console.log('over');
+    tl.play();
+  });
+
+  menu.addEventListener('mouseleave', (evt) => {
+    console.log('leave');
+    tl.reverse();
   });
 }
 
-if(innerItems.length) {
-  innerItems.forEach(item => {
-    item.addEventListener('click', (evt) => {
+if(inners) {
+  const onClickToggleNavItemHandler = (evt) => {
+    evt.stopPropagation();
+    const target = evt.target;
+    console.log(evt.target, evt.currentTarget);
+    if(target.classList.contains('top-multilevel-menu__expander')) {
       evt.preventDefault();
-      evt.currentTarget.closest('.top-multilevel-menu__list-item').classList.toggle('expanded');
-    });
-  });
-}
 
-window.addEventListener('keydown', (evt) => {
-  if(evt.key === 'Enter' && evt.target.classList.contains('main-nav-item-expander')) {
-    evt.preventDefault();
-    const active = document.querySelector('.top-multilevel-menu__list-item.expanded');
-
-    if(active) {
-      active.classList.remove('expanded');
+      evt.currentTarget.classList.toggle('expanded');
     }
-
-    evt.target.closest('li').classList.toggle('expanded');
   }
-});
 
-window.addEventListener('click', (evt) => {
-  if(!evt.target.classList.contains('top-multilevel-menu') && window.innerWidth > 1239) {
-    document.querySelectorAll('.expanded').forEach(item => {
-      item.classList.remove('expanded');
-    })
-  }
-});
+  inners.forEach(inner => {
+    inner.addEventListener('click', onClickToggleNavItemHandler);
+  })
+}
