253735939b1e5daa70da6eac2ecc2cf19ff574ed
Yuriy Bevov <yuriybevov@gmail.com>
2025-05-13 21:28:41 +0300

    правки карты

--- src/pug/components/_ui/forms/calc-form/calc-form.js
+++ src/pug/components/_ui/forms/calc-form/calc-form.js
@@ -1,44 +1,44 @@
 const calc = document.querySelector(".calc-form");
 
 if (calc) {
-  const ctrls = calc.querySelectorAll("[data-price]");
-  const total = calc.querySelector(".calc__total-price span");
-  const basePrice = calc.querySelector("[data-base-price]");
+	const ctrls = calc.querySelectorAll("[data-price]");
+	const total = calc.querySelector(".calc-form__total-price span");
+	const basePrice = calc.querySelector("[data-base-price]");
 
-  let currentValue = Number(basePrice.dataset.basePrice.replaceAll(" ", ""));
+	let currentValue = Number(basePrice.dataset.basePrice.replaceAll(" ", ""));
 
-  const numberPrettify = (number) => {
-    const separator = " ";
-    return number
-      .toString()
-      .replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + separator);
-  };
+	const numberPrettify = (number) => {
+		const separator = " ";
+		return number
+			.toString()
+			.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + separator);
+	};
 
-  const fillPriceNode = (value) => {
-    total.innerHTML = numberPrettify(Math.round(value));
-  };
+	const fillPriceNode = (value) => {
+		total.innerHTML = numberPrettify(Math.round(value));
+	};
 
-  fillPriceNode(currentValue);
+	fillPriceNode(currentValue);
 
-  const setTotalPrice = () => {
-    let total = currentValue;
+	const setTotalPrice = () => {
+		let total = currentValue;
 
-    ctrls.forEach((ctrl) => {
-      if (ctrl.checked && ctrl.dataset.price) {
-        total += Number(ctrl.dataset.currentPrice)
-          ? Number(ctrl.dataset.currentPrice)
-          : Number(ctrl.dataset.price);
-      }
-    });
+		ctrls.forEach((ctrl) => {
+			if (ctrl.checked && ctrl.dataset.price) {
+				total += Number(ctrl.dataset.currentPrice)
+					? Number(ctrl.dataset.currentPrice)
+					: Number(ctrl.dataset.price);
+			}
+		});
 
-    fillPriceNode(total);
-  };
+		fillPriceNode(total);
+	};
 
-  ctrls.forEach((ctrl) => {
-    ctrl.addEventListener("click", () => {
-      if (ctrl.dataset.price) {
-        setTotalPrice();
-      }
-    });
-  });
+	ctrls.forEach((ctrl) => {
+		ctrl.addEventListener("click", () => {
+			if (ctrl.dataset.price) {
+				setTotalPrice();
+			}
+		});
+	});
 }
--- src/pug/components/_ui/yMaps/__yMaps.js
+++ src/pug/components/_ui/yMaps/__yMaps.js
@@ -3,6 +3,7 @@ const map = document.querySelector("#y-maps");
 if (map) {
 	let myMap = null;
 	const responsiveWidth = 960;
+	// const tabSwitchers = document.querySelectorAll('.tabs-switcher');
 
 	window.addEventListener("load", () => {
 		ymaps.ready(init);
@@ -13,46 +14,6 @@ if (map) {
 
 		let isMobileView = window.innerWidth >= responsiveWidth ? false : true;
 		let isDesktopView = window.innerWidth >= responsiveWidth ? true : false;
-
-		const BalloonLayout = ymaps.templateLayoutFactory.createClass(
-			'<div class="balloon-overlay"></div>' +
-				'<div class="custom-balloon">' +
-				'<div class="custom-baloon-content">' +
-				"$[properties.balloonContent]" +
-				"</div>" +
-				"</div>",
-			{
-				build: function () {
-					BalloonLayout.superclass.build.call(this);
-
-					// Блокируем взаимодействие с картой
-					myMap.behaviors.disable(["drag", "dblClickZoom"]);
-
-					const closeButton =
-						this.getElement().querySelector(".balloon-closer");
-					const overlay =
-						this.getElement().parentElement.querySelector(".balloon-overlay");
-
-					// Обработчик закрытия по крестику
-					closeButton.addEventListener("click", () => {
-						myMap.balloon.close();
-					});
-
-					// Обработчик закрытия по оверлею
-					overlay.addEventListener("click", (e) => {
-						if (e.target === overlay) {
-							myMap.balloon.close();
-						}
-					});
-				},
-
-				clear: function () {
-					// Восстанавливаем взаимодействие с картой
-					myMap.behaviors.enable(["drag", "dblClickZoom"]);
-					BalloonLayout.superclass.clear.call(this);
-				},
-			},
-		);
 
 		const setView = (map, view) => {
 			let coords = null;
@@ -72,6 +33,7 @@ if (map) {
 			map.container.fitToViewport();
 		};
 
+		// Создание карты.
 		myMap = new ymaps.Map(
 			"y-maps",
 			{
@@ -100,75 +62,121 @@ if (map) {
 		);
 
 		options.placemarks.forEach((placemark, index) => {
+			// Метка
 			const pin = new ymaps.Placemark(
 				JSON.parse("[" + placemark.placemarkCoords + "]"),
 				{
 					iconContent: placemark.placemarkContent
 						? `<div class="ymaps-icon-content-layout-inner">${placemark.placemarkContent}</div>`
 						: "",
-					balloonContent: placemark.balloonContent || "",
 				},
 				{
+					// Опции.
+					// Необходимо указать данный тип макета.
 					placemarkID: index,
 					iconLayout: "default#imageWithContent",
+					// Своё изображение иконки метки.
 					iconImageHref: placemark.iconPath,
+					// Размеры метки.
 					iconImageSize: [50, 50],
+					// Смещение левого верхнего угла иконки относительно
+					// её "ножки" (точки привязки).
 					iconImageOffset: [-25, -25],
 					iconContentOffset: [50, 10],
 					iconContentLayout: MyIconContentLayout,
-					balloonLayout: BalloonLayout,
-					balloonCloseButton: false,
-					balloonPanelMaxMapArea: 0,
 				},
 			);
 
 			myMap.geoObjects.add(pin);
 
-			pin.events.add("click", function (e) {
-				const target = e.get("target");
-				const coords = target.geometry.getCoordinates();
-
-				// Закрываем предыдущий балун
-				myMap.balloon.close();
-				target.balloon.open(coords);
+			pin.events.add("click", (evt) => {
+				evt.preventDefault();
+				console.log("test");
+				// myMap.setZoom(options.maxZoom).then(() => {
+				// const id = evt.get("target").options.get("placemarkID");
+				// tabSwitchers.forEach((switcher) => {
+				// 	if (Number(switcher.dataset.tab) === Number(id)) {
+				// 		switcher.click();
+				// 	}
+				// });
+				// });
 			});
 		});
 
+		// const tabs = document.querySelectorAll(".tabs-switcher");
+		// tabs.forEach((tab) => {
+		// 	tab.addEventListener("click", () => {
+		// 		const id = tab.dataset.tab;
+
+		// 		myMap.geoObjects.each((placemark) => {
+		// 			if (Number(placemark.options.get("placemarkID")) === Number(id)) {
+		// 				myMap.setZoom(options.maxZoom).then(() => {
+		// 					myMap.panTo(placemark.geometry.getCoordinates(), { duration: 0 });
+		// 					myMap.container.fitToViewport();
+		// 				});
+		// 			}
+		// 		});
+		// 	});
+		// });
+
+		// ZOOM-CONTROL
 		let ZoomLayout = ymaps.templateLayoutFactory.createClass(
+			//Шаблон html кнопок зума
+			// "<button id='zoom-refresh' class='zoom-btn zoom-btn-refresh' aria-label='Вернуть карту в первоначальное состояние'></button>" +
 			"<div class='zoom-btns'>" +
-				"<button id='zoom-in' class='zoom-btn zoom-btn-in'></button>" +
-				"<button id='zoom-out' class='zoom-btn zoom-btn-out'></button>" +
+				"<button id='zoom-in' class='zoom-btn zoom-btn-in' aria-label='Увеличить масштаб'></button>" +
+				"<button id='zoom-out' class='zoom-btn zoom-btn-out' aria-label='Уменьшить масштаб'></button>" +
 				"</div>",
 			{
+				// Переопределяем методы макета, чтобы выполнять дополнительные действия
+				// при построении и очистке макета.
 				build: function () {
+					// Вызываем родительский метод build.
 					ZoomLayout.superclass.build.call(this);
+
+					// Привязываем функции-обработчики к контексту и сохраняем ссылки
+					// на них, чтобы потом отписаться от событий.
 					this.zoomInCallback = ymaps.util.bind(this.zoomIn, this);
 					this.zoomOutCallback = ymaps.util.bind(this.zoomOut, this);
 
+					// Начинаем слушать клики на кнопках макета.
 					let zoomInBtn = document.getElementById("zoom-in");
 					let zoomOutBtn = document.getElementById("zoom-out");
+					// let zoomRefreshBtn = document.getElementById("zoom-refresh");
 
 					zoomInBtn.addEventListener("click", this.zoomInCallback);
 					zoomOutBtn.addEventListener("click", this.zoomOutCallback);
+
+					// zoomRefreshBtn.addEventListener("click", () => {
+					// 	tabSwitchers[0].click();
+					// 	const center = isDesktopView
+					// 		? JSON.parse("[" + options.desktopViewCenter + "]")
+					// 		: JSON.parse("[" + options.mobileViewCenter + "]");
+					// 	const zoom = Number(options.zoom);
+					// 	myMap.setZoom(zoom).then(() => {
+					// 		myMap.panTo(center, { duration: 0 });
+					// 	});
+					// });
 				},
 
 				clear: function () {
-					let zoomInBtn = document.getElementById("zoom-in");
-					let zoomOutBtn = document.getElementById("zoom-out");
-
+					// Снимаем обработчики кликов.
 					zoomInBtn.removeEventListener("click", this.zoomInCallback);
 					zoomOutBtn.removeEventListener("click", this.zoomOutCallback);
+					// Вызываем родительский метод clear.
 					ZoomLayout.superclass.clear.call(this);
 				},
 
 				zoomIn: function () {
 					myMap.balloon.close();
+
 					let map = this.getData().control.getMap();
 					map.setZoom(map.getZoom() + 1, { checkZoomRange: true });
 				},
 
 				zoomOut: function () {
 					myMap.balloon.close();
+
 					let map = this.getData().control.getMap();
 					map.setZoom(map.getZoom() - 1, { checkZoomRange: true });
 				},
